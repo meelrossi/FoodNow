42af6aa8d52d8e4e46b43818e857af87e6f17c70
Meli Rossi <merossi@itba.edu.ar>
2015-09-03 19:10:10 -0300

    first commit

---
+++ src/main/java/ar/edu/itba/it/paw/model/Order.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.it.paw.model;
+
+import java.awt.List;
+
+public class Order {
+	User user;
+	String address;
+	List<Dish> dishes;
+	double total;
+	Restaurant restaurant;
+}
---
+++ src/main/java/ar/edu/itba/it/paw/model/Restaurant.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.it.paw.model;
+
+import java.awt.List;
+
+public class Restaurant {
+	List<Menu> menu;
+	double minAmount;
+	String description;
+	String name;
+	String address;
+}
